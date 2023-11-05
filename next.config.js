b5f5c1f0315f91ad374398ab393119515ff2775c
pierrbt <pierre08bt@gmx.co.uk>
2023-11-05 15:56:04 +0100

    fix: headers

--- next.config.js
+++ next.config.js
@@ -2,32 +2,32 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'Content-Security-Policy',
-                        value:
-                            "default-src 'self' 'unsafe-inline' https://acabou.veagle.fr;",
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'https://acabou.veagle.fr',
-                    },
-                ],
-            },
-        ];
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/(.*)',
+    //             headers: [
+    //                 {
+    //                     key: 'X-Frame-Options',
+    //                     value: 'DENY',
+    //                 },
+    //                 {
+    //                     key: 'Content-Security-Policy',
+    //                     value:
+    //                         "default-src 'self' 'unsafe-inline' https://acabou.veagle.fr;",
+    //                 },
+    //                 {
+    //                     key: 'X-Content-Type-Options',
+    //                     value: 'nosniff',
+    //                 },
+    //                 {
+    //                     key: 'Access-Control-Allow-Origin',
+    //                     value: 'https://acabou.veagle.fr',
+    //                 },
+    //             ],
+    //         },
+    //     ];
+    // },
 
 }
 
